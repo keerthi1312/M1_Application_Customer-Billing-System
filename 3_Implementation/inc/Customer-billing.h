d08ea922036b074dd0baeeea498214b76c7e4325
Keerthana A <68195812+keerthi1312@users.noreply.github.com>
2021-11-23 15:07:57 +0530

    Update Customer-billing.h

--- 3_Implementation/inc/Customer-billing.h
+++ 3_Implementation/inc/Customer-billing.h
@@ -1,5 +1,5 @@
 /**
- * @file game.h
+ * @file customer billing.h
  * @author keerthi
  * @brief 
  * @version 0.1
@@ -8,7 +8,7 @@
  * @copyright Copyright (c) 2021
  * 
  */
- #include<stdio.h>
- #include<conio.h>
- #include<stdlib.h>
+ #include<stdio.h> ///for input output functions like printf, scanf
+ #include<conio.h> ///for console input ouput
+ #include<stdlib.h> ///for dynamic memory allocation and other standard functions
 
