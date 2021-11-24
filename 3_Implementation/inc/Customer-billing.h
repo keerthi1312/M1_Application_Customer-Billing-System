0afa060ef30bfa36eb5ea37c47ff0050e215255f
Keerthana A <68195812+keerthi1312@users.noreply.github.com>
2021-11-24 12:34:33 +0530

    Update Customer-billing.h

--- 3_Implementation/inc/Customer-billing.h
+++ 3_Implementation/inc/Customer-billing.h
@@ -9,6 +9,8 @@
  * 
  */
  #include<stdio.h> ///for input output functions like printf, scanf
- #include<conio.h> ///for console input ouput
  #include<stdlib.h> ///for dynamic memory allocation and other standard functions
+ #include<windows.h> /// Windows-specific header file for the C and C++
+ #include<string.h>		///defines one variable type, one macro, and various functions for manipulating arrays of characters
+
 
